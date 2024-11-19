dbba73e31d968c3f7c833307f3ed6e3714fb7eff
Eduardo Portes <epcduas05@hotmail.com>
2024-11-19 18:27:25 -0300

    Entidade Funcionario

---
+++ src/main/java/com/portes/jwt_test/entities/Funcionario.java
@@ -0,0 +1,101 @@
+package com.portes.jwt_test.entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Funcionario {
+
+    private int id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String telefone;
+    private String cargo;
+
+    public Funcionario() {
+
+    }
+
+    public Funcionario(int id, String nome, String cpf, String email, String telefone, String cargo) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.telefone = telefone;
+        this.cargo = cargo;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Funcionario other = (Funcionario) obj;
+        if (id != other.id)
+            return false;
+        return true;
+    }
+
+    
+
+    
+}
